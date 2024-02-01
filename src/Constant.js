70e2fab80e58992d84978c6d538c849384cdaf1b
akdon1198 <akdon1198@gmail.com>
2024-02-01 09:50:36 +0530

    commited

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,4 @@
-export const commonapiurl = "http://localhost:5000/api/"
+export const commonapiurl = "https://quiz-backend-api-bn0x.onrender.com/api/"
 
 export const montharr = [
     "jan", "feb", "march", "june", "july", "aug", "sep", "oct",
